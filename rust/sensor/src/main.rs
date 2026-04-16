be9dc10185d757ddd02a4ba9083c2c99b017f78a
Natita-a <carolito133@gmail.com>
2026-04-15 18:15:30 -0600

    Update main.rs

--- rust/sensor/src/main.rs
+++ rust/sensor/src/main.rs
@@ -1,1 +1,31 @@
-fn main() {}
+use rand::Rng;
+use serde::Serialize;
+use std::{thread, time::Duration};
+
+#[derive(Serialize)]
+struct SensorReading {
+    sensor_id: String,
+    value: f32,
+}
+
+#[tokio::main]
+async fn main() {
+    loop {
+        let value: f32 = rand::thread_rng().gen_range(20.0..30.0);
+
+        let reading = SensorReading {
+            sensor_id: "sensor-1".to_string(),
+            value,
+        };
+
+        println!("Sensor enviando: {:?}", value);
+
+        let _ = reqwest::Client::new()
+            .post("http://edge:3001/data")
+            .json(&reading)
+            .send()
+            .await;
+
+        thread::sleep(Duration::from_secs(2));
+    }
+}
